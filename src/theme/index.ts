14392d730acbf67a4a7f9964b6accc4cf1970806
Yena-Yun <cheryleduart@gmail.com>
2022-01-22 21:14:34 +0900

    8강 실습

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,5 +1,25 @@
 import { extendTheme, theme as base, withDefaultColorScheme, withDefaultVariant } from '@chakra-ui/react';
 
+const inputSelectStyles = {
+	variants: {
+		filled: {
+			field: {
+				_focus: {
+					borderColor: 'brand.500'
+				}
+			}
+		}
+	},
+	sizes: {
+		md: {
+			field: {
+				borderRadius: 'none'
+			}
+		}
+			
+	}
+}
+
 const theme = extendTheme({
 	colors: {
 		brand: {
@@ -18,7 +38,22 @@ const theme = extendTheme({
 	fonts: {
 		heading: `Montserrat, ${base.fonts?.heading}`,
 		body: `Inter, ${base.fonts?.body}`,
-	}
+	},
+	components: {
+		Input: { ...inputSelectStyles },
+		Select: { ...inputSelectStyles },
+		Checkbox: {
+			baseStyle: {
+				control: {
+					borderRadius: 'none',
+					_focus: {
+						ring: 2,
+						ringColor: 'brand.500'
+					},
+				},
+			},
+		},
+	},
 },
 	withDefaultColorScheme({
 		colorScheme: 'brand',
